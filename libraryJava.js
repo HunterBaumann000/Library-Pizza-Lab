ce6dcba1c8447b0fefa89f2e07f6a7f6caf0c5ba
HunterBaumann000 <71730976+HunterBaumann000@users.noreply.github.com>
2020-10-26 19:52:03 -0500

    10/26

---
+++ libraryJava.js
@@ -0,0 +1,29 @@
+/*
+tests
+
+    <label>Enter The Amount Of Late Books: </label>
+
+    <input type="number" id="BooksTxtBox" value="" />
+
+    <br> <br>
+
+    <label>Enter The Days Late: </label>
+
+    <input type="number" id="BooksLateTxtBox" value="" />
+
+    <br> <br> <br>
+
+    <label>Enter The Amount Of Late DVDS: </label>
+
+    <input type="number" id="DVDTxtBox" value="" />
+
+    <br> <br>
+
+    <label>Enter The Days Late: </label>
+
+    <input type="number" id="DVDsLateTxtBox" value="" />
+
+    <br> <br>
+
+    <button onclick="CalculateTotal()">Click This Button to Calculate Total!</button>
+*/
